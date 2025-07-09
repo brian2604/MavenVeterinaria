e984540b343e9f683ffd915e831de4e72d9b80b6
brian2604 <quinterobrian2403@gmail.com>
2025-07-08 21:45:15 -0500

    Update veterinaria.sql

--- hibernate/veterinaria.sql
+++ hibernate/veterinaria.sql
@@ -2,10 +2,10 @@ CREATE DATABASE veterinaria;
 USE veterinaria;
 
 CREATE TABLE mascotas (
-id_mascota INT(11) NOT NULL,
+id INT(11) NOT NULL,
 nombre VARCHAR(45) DEFAULT NULL,
 raza VARCHAR(45) DEFAULT NULL,
 color VARCHAR(45) DEFAULT NULL,
 sexo VARCHAR(45) DEFAULT NULL,
-PRIMARY KEY (id_mascota)
+PRIMARY KEY (id)
 );
